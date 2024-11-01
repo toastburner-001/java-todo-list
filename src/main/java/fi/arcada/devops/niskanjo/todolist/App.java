2a1c05877d65616e96789f75fc77b58146a4c71c
niskanjo <niskanjo@arcada.fi>
2024-11-01 19:38:23 +0200

    Initial commit

---
+++ src/main/java/fi/arcada/devops/niskanjo/todolist/App.java
@@ -0,0 +1,10 @@
+package fi.arcada.devops.niskanjo.todolist;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
